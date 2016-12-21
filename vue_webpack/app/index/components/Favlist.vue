69b9215f6c32835db9daad33aa2daf39c8fb5375
00ERNEST00 <ernest_ma@sina.com>
2016-12-21 10:09:14 +0800

    first commit

---
+++ vue_webpack/app/index/components/Favlist.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-for="n in 10">{{msg}}</div>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style>
+    html{
+        background: cadetblue;
+    }
+</style>
